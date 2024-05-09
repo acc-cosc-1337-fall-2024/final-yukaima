2267305eb07978a9db5c5769e55b39bb0b4f683c
yukaima <157077934+yukaima@users.noreply.github.com>
2024-05-09 00:53:14 +0000

    Question 2

--- src/roll.cpp
+++ src/roll.cpp
@@ -1,1 +1,12 @@
 //
+#include "roll.h"
+
+void Roll::roll_dice()
+{
+    rolled_value = die1.roll() + die2.roll();
+}
+
+int Roll::roll_value() const
+{
+    return rolled_value;
+}
--- test/craps_test.cpp
+++ test/craps_test.cpp
@@ -3,10 +3,11 @@
 
 #include "die.h"
 #include <time.h>
+#include "roll.h"
 
 
 
-TEST_CASE("Test roll() function return a random number from 1 to 6")
+TEST_CASE("Test  die return a random number from 1 to 6")
 {
 	
 	Die die;
@@ -15,6 +16,21 @@ TEST_CASE("Test roll() function return a random number from 1 to 6")
 	{
 		int rollresult = die.roll();
 		bool result = rollresult >= 1 && rollresult <= 6;
+		REQUIRE(result == true) ;
+	}
+	
+}
+
+TEST_CASE("Test roll return a random number from 2 to 12")
+{
+	Die die1,die2;
+	Roll roll(die1,die2);
+    
+	for (int i = 0; i < 10; i++)
+	{
+		roll.roll_dice();
+		int rollvalue = roll.roll_value();
+		bool result = rollvalue >= 2 && rollvalue <= 12;
 		REQUIRE(result == true) ;
 	}
 	
